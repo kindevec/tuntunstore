9d0eb731674ded586a5ac214b9a057e823e08d18
miyashiromc <mkmcmiyako@gmail.com>
2026-08-07 22:08:01 -0500

    fix(ui): import useEffect in AdminPanel

--- src/components/AdminPanel.tsx
+++ src/components/AdminPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Order, OrderStatus, Product, EmailAlertConfig, UserProfile } from '../types';
 import { DiamondIcon } from './DiamondIcon';
 import { 
